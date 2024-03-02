8da451619773fde2596bf55d4d6ba114b414ce29
melih şahin <melihsahin24@gmail.com>
2024-03-02 22:59:13 +0300

    Add component and storybook

---
+++ src/stories/TextGradient.stories.js
@@ -0,0 +1,81 @@
+import {TextGradient} from "../components/TextGradient";
+
+export default {
+  component: TextGradient
+};
+
+export const Linear = {
+  args: {
+    color1: "#121fcf",
+    color2: "#13cf50",
+    startPosition:"0",
+    endPosition:"100",
+    type: "linear",
+    text: 'Linear gradient text',
+  }, argTypes: {
+    color1: {control: "color"},
+    color2: {control: "color"},
+  }
+};
+
+export const Radial = {
+  args: {
+    style: "",
+    color1: "#121fcf",
+    color2: "#13cf50",
+    startPosition:"0",
+    endPosition:"100",
+    type: "radial",
+    text: "Radial gradient text"
+  }, argTypes: {
+    color1: {control: "color"},
+    color2: {control: "color"},
+  }
+}
+
+export const Elliptical = {
+  args: {
+    style: "",
+    color1: "#121fcf",
+    color2: "#13cf50",
+    startPosition:"0",
+    endPosition:"100",
+    type: "elliptical",
+    text: "Elliptical gradient text"
+  }, argTypes: {
+    color1: {control: "color"},
+    color2: {control: "color"},
+  }
+}
+
+
+export const RepeatLinear = {
+  args: {
+    style: "",
+    color1: "#121fcf",
+    color2: "#13cf50",
+    startPosition:"0",
+    endPosition:"100",
+    type: "repeat-linear",
+    text: "Repeat linear gradient text"
+  }, argTypes: {
+    color1: {control: "color"},
+    color2: {control: "color"},
+  }
+}
+
+export const RepeatRadial = {
+  args: {
+    style: "",
+    color1: "#121fcf",
+    color2: "#13cf50",
+    startPosition:"0",
+    endPosition:"100",
+    type: "repeat-radial",
+    text: "Repeat radial gradient text"
+  }, argTypes: {
+    color1: {control: "color"},
+    color2: {control: "color"},
+  }
+}
+
